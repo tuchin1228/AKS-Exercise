a3d2cf152361da5153769e60cf2acddc062289a0
tuchin1228 <jhinwei1228@gmail.com>
2023-03-21 14:44:22 +0800

    init

---
+++ BackendApp/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                result = "Test"
+            });
+        }
+    }
+}
